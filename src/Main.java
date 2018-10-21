99b33a54313f0437e76a1fb15539294d77529771
AlexandraKoz <alejandrakoz@mail.ru>
2018-10-21 23:50:35 +0300

    used all the methods.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Simple<String> clothes = new MyArray<>();
-        clothes.myAdd("майка");
-        clothes.myAdd("штаны");
-        clothes.myAdd("кофта");
-        clothes.myAdd("носки");
+        clothes.myAdd("1. майка");
+        clothes.myAdd("2. штаны");
+        clothes.myAdd("3. кофта");
+        clothes.myAdd("4. носки");
+        System.out.println(clothes.mySize());
+        clothes.myPrintList();
+        clothes.myDelete(2);
+        clothes.myPrintList();
+        clothes.myGet(2);
         clothes.myPrintList();
     }
 }
