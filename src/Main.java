b9b599281eef8ffd0928bb0cfcd2d111a8873946
AlexandraKoz <alejandrakoz@mail.ru>
2018-10-23 21:08:45 +0300

    checked the work of all methods.

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,12 @@ public class Main {
         clothes.myAdd(new Clothes("майка","белый",42,500));
         clothes.myAdd(new Clothes("штаны","синий",44,590));
         clothes.myAdd(new Clothes("кофта,","черный",48,570));
+        clothes.myAdd(new Clothes("носки","розовый",46,800));
+        clothes.myPrintList();
+        System.out.println(clothes.mySize());
+        System.out.println(clothes.myGet(2));
+        clothes.myDelete(1);
+        clothes.myUpdate(0,new Clothes("худи","багровый",50,600));
         clothes.myPrintList();
     }
 }
